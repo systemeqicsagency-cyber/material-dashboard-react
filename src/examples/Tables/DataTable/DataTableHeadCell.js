4d94af879a62fd1a3522def56c581677c59df2d4
systemeqicsagency-cyber <systeme.qics.agency@gmail.com>
2025-10-03 19:21:27 +0200

    Updated DataTableHeadCell.js

--- src/examples/Tables/DataTable/DataTableHeadCell.js
+++ src/examples/Tables/DataTable/DataTableHeadCell.js
@@ -30,15 +30,7 @@ function DataTableHeadCell({ width, children, sorted, align, ...rest }) {
   const { darkMode } = controller;
 
   return (
-    <MDBox
-      component="th"
-      width={width}
-      py={1.5}
-      px={3}
-      sx={({ palette: { light }, borders: { borderWidth } }) => ({
-        borderBottom: `${borderWidth[1]} solid ${light.main}`,
-      })}
-    >
+    <MDBox component="th" width={width} py={1.5} px={3}>
       <MDBox
         {...rest}
         position="relative"
